4273111fc6c91b0e2766fe91ec1f95aae7abd090
kincki <kincki@users.noreply.github.com>
2018-06-25 13:48:01 +0300

    Add files via upload

--- 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
+++ 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
@@ -6,9 +6,15 @@ param (
     [string]$fileToInstall
 )
 
-<#
+
 $LogFile = "c:\apps\logs\$(gc env:computername).log"
-New-Item -ItemType file -Path $LogFile
+
+#The log file may already exist
+try {
+	New-Item -ItemType file -Path $LogFile -ErrorAction Stop
+} catch [System.IO.IoException] {
+	WriteLog "$LogFile already exists..Continue!.."
+}
 
 Function WriteLog
 {
@@ -16,26 +22,21 @@ Function WriteLog
 
 	Add-content $LogFile -value $logString
 }
-#>
 
-Write-Verbose "DEEPNETWORK new folder name has been created " -verbose
+WriteLog "~~~~~ DEEPNETWORK custom script running!!! ~~~~~" 
 
 
 $source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
 $dest = "C:\WindowsAzure\$folderName"
-New-Item -Path $dest -ItemType directory
-Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
 
-<#
 try{
-	New-Item -Path $dest -ItemType directory -ea stop
+	New-Item -Path $dest -ItemType directory -ErrorAction Stop
 } catch {
 	$ErrorMessage = $_.Exception.Message
     $FailedItem = $_.Exception.ItemName
 
 	WriteLog "ErrorMessage: $ErrorMessage"
 	WriteLog "FailedItem: $FailedItem"
-
 }
-#>
 
+Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
