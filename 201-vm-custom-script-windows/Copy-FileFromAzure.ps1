7b4bce8698cd12fb45d02e33052d3a756d84904f
kincki <kincki@users.noreply.github.com>
2018-06-25 11:38:14 +0300

    Add files via upload

--- 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
+++ 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
@@ -6,7 +6,29 @@ param (
     [string]$fileToInstall
 )
 
+$LogFile = "c:\apps\logs\$(gc env:computername).log"
+New-Item -ItemType file -Path $LogFile
+
+Function WriteLog
+{
+	Param ([string] $logString)
+
+	Add-content $LogFile -value $logString
+}
+
 $source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
 $dest = "C:\WindowsAzure\$folderName"
-New-Item -Path $dest -ItemType directory
+
+try{
+	New-Item -Path $dest -ItemType directory
+} catch {
+	$ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+
+	WriteLog "ErrorMessage: $ErrorMessage"
+	WriteLog "FailedItem: $FailedItem"
+
+}
+
+
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
